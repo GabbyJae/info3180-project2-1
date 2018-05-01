36ac00a7e3eb5fc9ea1ee8e512a55e892369f276
Stephan <stephanwitter@yahoo.com>
2018-05-01 05:30:51 +0000

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "itsasecret"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://stephan542:Project_2@localhost/UsersData"
+
+#For Heroku database which I pushed 
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://jpksuqatakngxa:52a669bd612ed3c38b8dde53dbd94a1baa592d66303dfcb2ed6c6119cf2e2689@ec2-107-20-249-68.compute-1.amazonaws.com:5432/dbtpli1m01eqh"
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://stephan542:Project_2@localhost/UsersData"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
--- flask-migrate.py
+++ flask-migrate.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+from flask_sqlalchemy import SQLAlchemy
+
 from app import app # we import the app object from the app module
 from app import db
 
@@ -12,4 +14,4 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
-    
+
